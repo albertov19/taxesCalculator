196976126f0b0ffa816deae3fcd7fede24592f37
Albertov19 <albertoviera@gmail.com>
2023-05-02 10:51:28 +0200

    type improvements

--- getTaxes.ts
+++ getTaxes.ts
@@ -6,6 +6,18 @@ import yargs from 'yargs';
 // Price data can be obtained as a CSV with the following URL (Changing Token Name):
 //https://coincodex.com/crypto/kusama/historical-data/
 
+// Interafaces, Types & Definitions
+interface PriceData {
+  [date: string]: number;
+}
+type Network = 'moonbeam' | 'moonriver' | 'polkadot' | 'kusama';
+const networkTokenTags: Record<Network, string> = {
+  moonbeam: 'GLMR',
+  moonriver: 'MOVR',
+  polkadot: 'DOT',
+  kusama: 'KSM',
+};
+
 // Inputs
 const args = yargs.options({
   networks: { type: 'string', demandOption: false, alias: 'n' },
@@ -17,7 +29,7 @@ const args = yargs.options({
   year: { type: 'number', demandOption: false, alias: 'y' },
 }).argv;
 
-// Networks and Addresses
+// Networks and Addresses Array
 const networks = args['networks'] ? args['networks'].split(',') : [];
 const addresses = args['addresses'] ? args['addresses'].split(',') : [];
 
@@ -25,7 +37,7 @@ const addresses = args['addresses'] ? args['addresses'].split(',') : [];
 const startDate = new Date(Date.UTC(args['year'], 0, 1));
 const endDate = new Date(Date.UTC(args['year'] + 1, 0, 1));
 
-async function main() {
+async function main(): Promise<void> {
   if (networks.length != addresses.length) {
     console.error('Address and network length must be the same!');
   }
@@ -35,24 +47,12 @@ async function main() {
   }
 }
 
-async function calculateForNetwork(network, address) {
-  // Fetch Price Data
-  let tokenTag;
-  switch (network.toLowerCase()) {
-    case 'moonbeam':
-      tokenTag = 'GLMR';
-      break;
-    case 'moonriver':
-      tokenTag = 'MOVR';
-      break;
-    case 'polkadot':
-      tokenTag = 'DOT';
-      break;
-    case 'kusama':
-      tokenTag = 'KSM';
-      break;
-    default:
-      console.error('Only supports polkadot, kusama, moonbeam and moonriver as input networks');
+async function calculateForNetwork(network: string, address: string): Promise<void> {
+  // Check if Token is Supported
+  const tokenTag = networkTokenTags[(network as Network).toLowerCase()];
+
+  if (!tokenTag) {
+    console.error('Only supports polkadot, kusama, moonbeam, and moonriver as input networks');
   }
 
   // Price File
@@ -65,55 +65,48 @@ async function calculateForNetwork(network, address) {
   }
 }
 
-async function generateJSON(priceFile, tokenTag) {
+async function generateJSON(priceFile: string, tokenTag: string): Promise<void> {
   // Load CSV Files
   const priceData = await csv().fromFile(`./CSVs/${priceFile}.csv`);
 
-  const priceJSON = [];
+  const priceJSON: PriceData = {};
   priceData.forEach((priceDateData) => {
-    const date = new Date(`${priceDateData['Date']}z`);
+    const date = new Date(`${priceDateData['Date']}Z`);
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const day = date.getDate().toString().padStart(2, '0');
     if (date >= startDate && date <= endDate) {
-      priceJSON.push({
-        date: `${year}-${month}-${day}`,
-        price: parseFloat(priceDateData['Close']),
-      });
+      priceJSON[`${year}-${month}-${day}`] = parseFloat(priceDateData['Close']);
     } else {
       console.log(`No Price Data for ${tokenTag} on date ${year}-${month}-${day}`);
     }
   });
 
-  // Sort and Save
-  priceJSON.sort((a, b) => (a.Date > b.Date ? 1 : -1));
+  // Save
   fs.writeFileSync(`./priceJSONs/${priceFile}.json`, JSON.stringify(priceJSON));
 
   console.log(`JSON Price File Generated for ${priceFile}`);
 }
 
-async function calculateData(priceFile, tokenTag, address) {
+async function calculateData(priceFile: string, tokenTag: string, address: string): Promise<void> {
   // Read JSON Price Data
   const priceDataBuffer = fs.readFileSync(path.resolve(`./priceJSONs/${priceFile}.json`));
   const priceDataJSONString = priceDataBuffer.toString();
-  const priceData = JSON.parse(priceDataJSONString);
+  const priceData: PriceData = JSON.parse(priceDataJSONString);
 
   // Read Staking Data
-  console.log(priceFile);
   const stakingData = await csv().fromFile(path.resolve(`./CSVs/${tokenTag}_${address}.csv`));
 
   let totalAmount: number = 0;
   stakingData.forEach((staking) => {
-    let stakingDate = new Date(`${staking['Date']}z`);
+    let stakingDate = new Date(staking['Date']);
+    const year = stakingDate.getFullYear();
+    const month = (stakingDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = stakingDate.getDate().toString().padStart(2, '0');
     if (stakingDate >= startDate && stakingDate <= endDate) {
-      const matchingData = priceData.find((priceDataDate) =>
-        priceDataDate['date'].startsWith(staking['Date'].slice(0, 10))
-      );
-      if (matchingData && !isNaN(matchingData.price)) {
-        totalAmount += parseFloat(staking.Value) * matchingData.price;
-      } else {
-        console.log(`No price matching for ${staking['Date']}`);
-      }
+      priceData[`${year}-${month}-${day}`]
+        ? (totalAmount += parseFloat(staking.Value) * priceData[`${year}-${month}-${day}`])
+        : console.log(`No price matching for ${`${year}-${month}-${day}`}`);
     }
   });
 
